b505642ffcd2cc80b8a221c2af813db01bd4b62d
BLACK-ASH <ashifshaikh9a@gmail.com>
2026-02-28 00:15:44 +0530

    Doctor Dashboard Added

--- frontend/app/dashboard/doctor/page.tsx
+++ frontend/app/dashboard/doctor/page.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { columns } from './_components/colunm'
+import { DataTable } from './_components/data-table'
 
-const page = () => {
+const page = async () => {
+
+  const data = [
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+    {
+      id: "string",
+      patient_id: "string",
+      test_type: "string",
+      status: "pending"
+    },
+  ]
+
+  const res = await fetch(process.env.BASE_URL + "/doctor/reports/pending")
+    .then(res => res.json())
+    .catch(err => console.error(err))
+
   return (
-    <div>
-      This Is Doctor Page
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={res.reports || data} />
     </div>
   )
 }
