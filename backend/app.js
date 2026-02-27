74351e3300f5a023075da4441d14a23d9f8e6d43
VivekPrajapati-Git <vivekbusiness20@gmail.com>
2026-02-27 17:52:00 +0530

    Added api for report ranking

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+// MongoDB Connection
+const connectMongo = require('./config/mongodb');
+connectMongo();
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
@@ -12,6 +16,9 @@ app.use('/api/presence', presenceRoutes);
 
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
+
+const reportRoutes = require('./routes/report');
+app.use('/api/report', reportRoutes);
 
 app.get('/', (req, res) => {
     res.send("Hello world");
