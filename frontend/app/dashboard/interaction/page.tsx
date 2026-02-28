301aa5f32c1b5c4bf4ec4004772faad7d1e5d7de
BLACK-ASH <ashifshaikh9a@gmail.com>
2026-02-28 10:31:49 +0530

    Interaction Updated

--- frontend/app/dashboard/interaction/page.tsx
+++ frontend/app/dashboard/interaction/page.tsx
@@ -1,15 +1,13 @@
-import { Params } from 'next/dist/server/request/params'
 import { NetworkGraph } from './_components/network-work'
 import testData from "./testData.json"
 
 const page = async () => {
-  const data = testData.data
-  const res = await fetch(process.env.BASE_URL + "/neo4j/patient/PFID_A_6436", { next: { revalidate: 90 } }).then(data => data.json())
-  return (
-    <div>
-      <NetworkGraph data={res.data ? res.data : data} />
-    </div>
-  )
+    const data = testData.data
+    return (
+        <div>
+            <NetworkGraph data={data} />
+        </div>
+    )
 }
 
 export default page
